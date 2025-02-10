84c708ebd2a7726cedffae0b60bb8ceca3f7f773
Muhammad Shoaib Khalid <shoaib_khalid@diyatech.org.pk>
2025-02-10 18:36:13 +0500

    added log4net functionality

--- Program.cs
+++ Program.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Configuration;
+using log4net;
+using log4net.Config;
 
 class Program
 {
+    private static readonly ILog log = LogManager.GetLogger(typeof(TcpServer));
     static void Main()
     {
+        ConfigureLogging();
         // See https://aka.ms/new-console-template for more information
         Console.WriteLine("Hello, Tcp-Server World!");
-        
+
         string port = ConfigurationManager.AppSettings["port"] ?? "8080";
         Console.WriteLine("Starting TCP-SERVER on Port: " + port);
+        log.Info($"Server is starting on port: [{port}]...");
         TcpServer server = new TcpServer(int.Parse(port), null);
         server.Start().Wait();
+    }
+    private static void ConfigureLogging()
+    {
+        var logRepository = LogManager.GetRepository(System.Reflection.Assembly.GetEntryAssembly());
+        XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
     }
 }
--- obj/Debug/net9.0/tcp-server.AssemblyInfo.cs
+++ obj/Debug/net9.0/tcp-server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tcp-server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9966e4520ac06906b6d73ba4f82e3c6872863b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4f162e087ed2ea6f4c4797377e15a83b3473b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("tcp-server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tcp-server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
